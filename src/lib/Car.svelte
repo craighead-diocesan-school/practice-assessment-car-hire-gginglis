b4acf9501d224c8695f64d52aba9da41ee79cb8c
gginglis <inglisge23@craighead.school.nz>
2024-08-11 17:02:31 +1200

    displayed items on page

---
+++ src/lib/Car.svelte
@@ -0,0 +1,55 @@
+<script>
+    export let car;
+    export let description;
+    export let price;
+    export let img;
+    export let credit;
+    // export let available;
+
+    // async function getCars() {
+    //     let shopData = await fetch(
+    //         "https://digitech.craighead.school.nz/api/car-hire",
+    //     );
+    //     return shopData.json();
+    // }
+
+    // let current = cars[0];
+</script>
+
+<head>
+    <style>
+        @import url("https://fonts.googleapis.com/css2?family=Audiowide&display=swap");
+    </style>
+</head>
+
+<main>
+    <img src={img} alt="" title={credit} />
+    <div class="info">
+        <h2>{car}</h2>
+        <h3>${price}</h3>
+        <p>{description}</p>
+    </div>
+</main>
+
+<style>
+    main {
+        display: flex;
+    }
+    .info {
+        margin: 30px;
+    }
+    img {
+        width: 400px;
+    }
+
+    h2 {
+        color: #131415;
+        font-family: "Audiowide", sans-serif;
+        font-weight: 500;
+        font-size: 1.6rem;
+    }
+
+    h3 {
+        color: #131415;
+    }
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,13 +1,49 @@
 <script>
-  import Header from '$lib/Header.svelte'
+  import Header from "$lib/Header.svelte";
+  import Car from "$lib/Car.svelte";
+
+  async function getCars() {
+    let shopData = await fetch(
+      "https://digitech.craighead.school.nz/api/car-hire",
+    );
+
+    return shopData.json();
+  }
+
+  let cars = getCars();
 </script>
 
 <Header />
 
 <main>
-  <h2>SvelteKit</h2>
-
-  <p>Welcome to coding with SvelteKit, a modern JavaScript framework that makes it easy to code great apps.</p>
+  {#await cars}
+    waiting...
+  {:then cars}
+    {#each cars.fast as car}
+      {#if car.available}
+        <Car
+          car={car.car}
+          description={car.description}
+          price={car.price}
+          img={car.img}
+          credit={car.credit}
+          available={car.available}
+        />
+      {/if}
+    {/each}
+    {#each cars.nice as car}
+      {#if car.available}
+        <Car
+          car={car.car}
+          description={car.description}
+          price={car.price}
+          img={car.img}
+          credit={car.credit}
+          available={car.available}
+        />
+      {/if}
+    {/each}
+  {/await}
 </main>
 
 <footer>
